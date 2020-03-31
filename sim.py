989b72b6f97234df4a1f0a44b808685782dd44eb
Sanjit Raman <sanjit.raman@gmail.com>
2020-03-31 13:23:36 +0100

    Added Git help

--- sim.py
+++ sim.py
@@ -1,1 +1,20 @@
+# Covid-19 SIR Simulation
+# Date: 31.03.2020
+# Version 1.0
+
+
+# I pull the latest code using:
+# git pull https://github.com/SanjitRaman/Covid-19-sim.git
+# Then I can make changes on Visual Studio Code, as I please.
+
+# When I want to add them to the staging (preparation) area on my local computer:
+# git add <filename> or git add . to add all changes.
+
+# When I want to commit them to the timeline,
+# git commit -m "Enter a commit message"
+
+# To upload the changes back to github:
+# git push -u origin master
+
+#START OF PROGRAM
 print('Hello COVID 19!')
